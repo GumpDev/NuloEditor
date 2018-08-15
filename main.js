d60f5d74edb755ef8d39536d669c536a31e873bb
GumpFlash <gump.flash@gmail.com>
2018-08-15 09:55:15 -0300

    Revert "2"
    This reverts commit 05e444eb0765f4887f52c249722cbd9cb1e05dd1.

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var path = require('path');
 let mainWindow = null;
 
 app.on('ready', () =>{
-    mainWindow = new BrowserWindow({frame: false,width: 900, height: 600, icon: path.join(__dirname, '/icon.png')});const {app, Menu} = require('electron')
+    mainWindow = new BrowserWindow({frame: false,width: 900, height: 600, icon: path.join(__dirname, '/icon.png')});
+    mainWindow.setMenu(null);
     mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.webContents.openDevTools();
+    
 });

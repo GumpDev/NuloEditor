7cebeae56f86454aada19d3e314c5b48fa41c544
GumpFlash <gump.flash@gmail.com>
2018-08-16 17:57:10 -0300

    3

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const {app,BrowserWindow} = require('electron');
 var path = require('path');
 let mainWindow = null;
+const client = require('discord-rich-presence')('479737232413032479');
+ 
 
 app.on('ready', () =>{
     mainWindow = new BrowserWindow({frame: false,width: 900, height: 600, icon: path.join(__dirname, '/icon.png')});
     mainWindow.setMenu(null);
     mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.webContents.openDevTools();
-    
+
+
+    client.updatePresence({
+        state: 'Editing Codes',
+        //details: 'JavaScript',
+        largeImageKey: 'iconfundo',
+        largeImageText: 'NuloEditor',
+        //smallImageKey: 'js',
+        //smallImageText: 'JavaScript',
+        instance: true,
+      });
 });
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var dialog = app.dialog;
 var fs = require('fs');
 
 var filename = "";
-var contentcache = "";
+var type = "";
 
 function minimize(){
      var window = remote.getCurrentWindow();
@@ -27,9 +27,39 @@ function closeBtn(){
 }
 
 function save(){
+    if(filename == ""){
+        dialog.showSaveDialog((fileName) =>{
+            if(fileName == undefined){
+                return;   
+            }
+        
+            var content = myCodeMirror.getValue();
+
+            fs.writeFile(fileName,content,(err)=>{
+                if(err)console.log(err);
+                filename = fileName;
+                var num = fileName.split("\\").length - 1;
+                document.getElementById('title').innerText = fileName.split("\\")[num];
+                type = filename.split(".")[1];
+            });
+        });
+    }else{
+        var content = myCodeMirror.getValue();
+
+        fs.writeFile(filename,content,(err)=>{
+            if(err)console.log(err);
+            contentcache = content;
+            var num = filename.split("\\").length - 1;
+            document.getElementById('title').innerText = filename.split("\\")[num];
+            type = filename.split(".")[1];
+            setInterval(verify,500);
+        });
+    }
+}
+
+function saveAs(){
     dialog.showSaveDialog((fileName) =>{
         if(fileName == undefined){
-            alert("failed to Save");
             return;   
         }
     
@@ -37,16 +67,82 @@ function save(){
 
         fs.writeFile(fileName,content,(err)=>{
             if(err)console.log(err);
-            alert("The File has been Saved!");
-            filename = fileName;
-            contentcache = content;
-            document.getElementById('title').innerText = fileName;
-            setInterval(verify,500);
         });
     });
 }
 
+function load(){
+    dialog.showOpenDialog((fileNames) =>{
+        if(fileName == undefined){
+            return;   
+        }else{
+            readFile(fileNames[0]);
+            document.getElementById('title').innerText = fileNames[0];
+        }
+    });
+}
+
+function readFile(filepath){
+    fs.readFile(filepath,'utf-8',(err,data) =>{
+        if(err){
+            alert(err);
+            return;
+        }
+
+        
+    });
+}
+
 function verify(){
-    if(myCodeMirror.getValue() !== contentcache)
-    document.getElementById('title').innerText = filename + "(unsaved)";
+    if(filename != ""){
+        var num = filename.split("\\").length - 1;
+        document.getElementById('title').innerText = filename.split("\\")[num]+ "(unsaved)";
+    }
+}
+
+function fileMenu(bool){
+    var tabMenu = document.getElementById('menu1');
+    if(bool){
+        EditMenu(false);
+        HelpMenu(false);
+        tabMenu.style.zIndex = 5;
+        tabMenu.style.opacity = 1;
+    }else{
+        tabMenu.style.zIndex = -2;
+        tabMenu.style.opacity = 0;
+    }
+}
+
+function EditMenu(bool){
+    var tabMenu = document.getElementById('menu2');
+    if(bool){
+        fileMenu(false);
+        HelpMenu(false);
+        tabMenu.style.zIndex = 5;
+        tabMenu.style.opacity = 1;
+    }else{
+        tabMenu.style.zIndex = -2;
+        tabMenu.style.opacity = 0;
+    }
+}
+
+function HelpMenu(bool){
+    var tabMenu = document.getElementById('menu3');
+    if(bool){
+        fileMenu(false);
+        EditMenu(false);
+        tabMenu.style.zIndex = 5;
+        tabMenu.style.opacity = 1;
+    }else{
+        tabMenu.style.zIndex = -2;
+        tabMenu.style.opacity = 0;
+    }
+}
+
+function quit(){
+    window.close();
+}
+
+function hub(){
+    window.location = "hub.html";
 }
